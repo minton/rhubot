7401f0f14748b86bfe27015c06733074ed996f81
Michael Minton <michaelcminton@gmail.com>
2013-03-27 19:51:52 -0400

    Run requested process.

--- rhubot.rb
+++ rhubot.rb
@@ -1,3 +1,7 @@
 class Rhubot
-  puts ARGV[1]
+  script = ARGV[1]
+  args = ARGV[2]
+  script_path = File.dirname(__FILE__) + "/scripts"
+  result = `ruby #{script_path}/#{script}.rb #{args}`.gsub /(?<!\n)\n(?!\n)/, ''
+  puts result
 end
